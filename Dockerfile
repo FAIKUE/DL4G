a4817bdc75c36a95854b8b8af0e8244ff237be7d
Christen Roland tcchrist <roland.christen@hslu.ch>
2019-07-27 20:09:27 +0200

    Features/new cicd

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine:3.10
+MAINTAINER Roli Christen <roland.christen@hslu.ch>
+COPY ./source /app/jass-kit
--- build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# pack sources
-mkdir -p dist
-tar cfv ./dist/libs.tar --exclude .git -C source/ .
